5ddbc3fdced9fb90b825185f4fa53bdb0f452d23
Chris Eckhardt <ceckhardt06@gmail.com>
2020-06-18 22:33:23 -0700

    Update vga.c

--- vga.c
+++ vga.c
@@ -35,10 +35,13 @@
 #define VGA_NUM_REGS        	(1 + VGA_NUM_SEQ_REGS + VGA_NUM_CRTC_REGS + \
 				VGA_NUM_GC_REGS + VGA_NUM_AC_REGS)
 
+#define BLACK 			0x00
+#define WHITE 			0x3F
+
 int current_color = 0x01;
 
 
-int g_window_id 	= 0;
+int g_window_id = 0;
 
 typedef struct _BOUND {
 	int x;
@@ -94,7 +97,13 @@ void write_regs (unsigned char * regs);
 
 void create_window ( PARAM_VGA_CREATE_WINDOW * params);
 
+void draw_pixel (PARAM_VGA_DRAW_PIXEL * params);
+
+ void draw_line (PARAM_VGA_DRAW_LINE * params);
+
 void clear_screen();
+
+void set_pixel (VGA_WINDOW * window, int x, int y, int color);
 
 void poke_pixel (int x, int y, int color);
 
@@ -105,6 +114,14 @@ void vga_draw_frame(VGA_WINDOW * window);
 void vga_draw_window(VGA_WINDOW * window);
 
 void vga_render_windows();
+
+int get_canvas_pixel(VGA_WINDOW * wnd, int x, int y);
+
+void set_canvas_pixel(VGA_WINDOW * wnd, int x, int y, int color);
+
+VGA_WINDOW * get_window(int id);
+
+int m_abs (int a);
 
 /* quadtree functions */
 
@@ -127,6 +144,8 @@ void subdivide(QNODE * q, BOUND b);
 int search_qtree(QNODE * q, int x, int y);
 
 BOUND get_intersection(BOUND * a, BOUND * b);
+
+int bound_contains_within(BOUND * in, BOUND * out);
 
 void add_window_to_list(VGA_WINDOW * w);
 
@@ -195,6 +214,7 @@ void vga_process (PROCESS proc, PARAM param)
 
             case VGA_CREATE_WINDOW:
                 create_window( (PARAM_VGA_CREATE_WINDOW *) &msg->u.create_window );
+
                 break;
 
             case VGA_DRAW_TEXT:
@@ -210,10 +230,8 @@ void vga_process (PROCESS proc, PARAM param)
                 break;
         }
 
-		/* build quadtrees */
-		build_quadtrees();
-
-		vga_render_windows();
+            build_quadtrees();
+    vga_render_windows();
 
         /* reply to sender */
         reply(sender);
@@ -275,6 +293,10 @@ void write_regs (unsigned char * regs)
     outportb (VGA_AC_INDEX, 0x20);
 }
 
+/**************************************************************
+ *                   API : CREATE WINDOW                      *
+ *************************************************************/
+
 void create_window ( PARAM_VGA_CREATE_WINDOW * params)
 {
 	VGA_WINDOW * window = malloc( sizeof(VGA_WINDOW) );
@@ -288,6 +310,8 @@ void create_window ( PARAM_VGA_CREATE_WINDOW * params)
 	window->canvas.bound.y = params->y;
 	window->canvas.bound.width = params->width;
 	window->canvas.bound.height = params->height;
+    window->canvas.buffer = malloc(
+        sizeof(int) * (window->canvas.bound.width) * (window->canvas.bound.height));
     window->color = current_color++;
 
     int bound_size = 1;
@@ -306,13 +330,126 @@ void create_window ( PARAM_VGA_CREATE_WINDOW * params)
     window->next = NULL;
     window->prev = NULL;
 	add_window_to_list(window);
+
 }
+
+/**************************************************************
+ *                    API : DRAW PIXEL                        *
+ *************************************************************/
+
+void draw_pixel (PARAM_VGA_DRAW_PIXEL * params)
+{
+    VGA_WINDOW * wnd = get_window(params->window_id);
+	if(wnd == NULL)
+		return;
+
+	set_canvas_pixel(wnd, params->x, params->y, params->color);
+
+    vga_draw_canvas(wnd);
+}
+
+ /*************************************************************
+ *                      API : DRAW TEXT                       * 
+ *************************************************************/
+
+ /*************************************************************
+ *                     API : DRAW LINE                        *
+ *************************************************************/
+
+void draw_line (PARAM_VGA_DRAW_LINE * params)
+{
+
+    VGA_WINDOW * wnd = get_window(params->window_id);
+	if(wnd == NULL)
+		return;
+
+	int x, y, dx, dy, dx1, dy1, px, py, xe, ye, i;
+
+	dx = params->x1 - params->x0;
+	dy = params->y1 - params->y0;
+
+	dx1 = m_abs(dx);
+	dy1 = m_abs(dy);
+
+	px = 2 * dy1 - dx1;
+	py = 2 * dx1 - dy1;
+
+	if (dy1 <= dx1) {
+
+		if (dx >= 0) {
+			x = params->x0;
+			y = params->y0;
+			xe = params->x1;
+		} else {
+			x = params->x1;
+			y = params->y1;
+			xe = params->x0;
+		}
+
+		set_canvas_pixel(wnd, x, y, params->color);
+
+		for (i = 0; x < xe; i++) {
+			x += 1;
+
+			if (px < 0) {
+				px = px + 2 * dy1;
+			} else {
+				if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0)) {
+					y += 1;
+				} else {
+					y -= 1;
+				}
+				px = px + 2 * (dy1 - dx1);
+			}
+
+			set_canvas_pixel(wnd, x, y, params->color);
+		}
+
+	} else {
+
+		if (dy >= 0) {
+			x = params->x0;
+			y = params->y0;
+			ye = params->y1;
+		} else {
+			x = params->x1;
+			y = params->y1;
+			ye = params->y0;
+		}
+
+		set_canvas_pixel(wnd, x, y, params->color);
+
+		for (i = 0; y < ye; i++) {
+			y += 1;
+
+			if (py < 0) {
+				py = py + 2 * dx1;
+			} else {
+				if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0)) {
+					x += 1;
+				} else {
+					x -= 1;
+				}
+				py = py + 2 * (dx1 - dy1);
+			}
+
+            set_canvas_pixel(wnd, x, y, params->color);
+			
+		}
+
+	}
+    vga_draw_canvas(wnd);
+}
+
+/**************************************************************
+ *                 PIXEL DRAWING UTILITIES                    *
+ *************************************************************/
 
 void clear_screen()
 {
 	for(int x = 0; x < SCREEN_WIDTH; x++)
 		for(int y = 0; y < SCREEN_HEIGHT; y++)
-			poke_pixel(x, y, 0x00);
+			poke_pixel(x, y, BLACK);
 }
 
 void set_pixel (VGA_WINDOW * window, int x, int y, int color)
@@ -322,9 +459,6 @@ void set_pixel (VGA_WINDOW * window, int x, int y, int color)
 	// check quadtree
 	if(search_qtree(window->root, x, y) == 0)
 		poke_pixel(x, y, color);
-
-
-	
 }
 
 void poke_pixel (int x, int y, int color)
@@ -332,41 +466,67 @@ void poke_pixel (int x, int y, int color)
 	poke_b( (VIDEO_BASE_ADDRESS + y * SCREEN_WIDTH + x), color);
 }
 
+int m_abs (int a) 
+{
+	return a < 0 ? -a : a;
+}
+
 /**************************************************************
  *               WINDOW MANAGEMENT FUNCTIONS                  *
  *************************************************************/
 
- void vga_render_windows()
- {
+VGA_WINDOW * get_window(int id)
+{
+    VGA_WINDOW * w_ptr = window_list_head;
+    while(w_ptr != NULL) {
+        if(w_ptr->id == id)
+            break;
+        w_ptr = w_ptr->next;
+    }
+    return w_ptr;
+}
+
+void vga_render_windows()
+{
     clear_screen();
-	if(!window_list_tail)
+    if(!window_list_tail)
         return;
 
-	VGA_WINDOW * w_ptr = window_list_tail;
+    VGA_WINDOW * w_ptr = window_list_tail;
     while(w_ptr != NULL) {
-		vga_draw_window(w_ptr);
+        vga_draw_window(w_ptr);
         w_ptr = w_ptr->prev;
-	}
-
- }
+    }
+}
 
 void vga_draw_frame(VGA_WINDOW * window)
 {
 	BOUND fb = window->frame.bound;
 
-	// draw banner
+	for(int x = fb.x; x < fb.x+fb.width; x++)
+		for(int y = fb.y; y < fb.y+1; y++)
+			set_pixel(window, x, y, window->color);
+    
+    for(int y = fb.y; y < fb.y+fb.height; y++)
+        set_pixel(window, fb.x, y, window->color);
 
-    // draw frame
-	int x2, y2;
-	for(x2 = fb.x; x2 < fb.x+fb.width; x2++)
-		for(y2 = fb.y; y2 < fb.y+fb.height; y2++)
-			set_pixel(window, x2, y2, window->color);
+    for(int y = fb.y; y < fb.y+fb.height; y++)
+        set_pixel(window, fb.x+fb.width-1, y, window->color);
 
+    for(int x = fb.x; x < fb.x+fb.width; x++)
+        set_pixel(window, x, fb.y+fb.height-1, window->color);
+
+    // draw text
 }
 
 void vga_draw_canvas(VGA_WINDOW * window)
 {
-    // draw canvas from buffer
+	BOUND cb = window->canvas.bound;
+
+	for(int x = 0; x < cb.width; x++)
+		for(int y = 0; y < cb.height; y++)
+			//set_pixel(window, x+cb.x, y+cb.y, get_canvas_pixel(window, x, y));
+            ;
 }
 
 void vga_draw_window(VGA_WINDOW * window)
@@ -388,6 +548,20 @@ void add_window_to_list(VGA_WINDOW * w)
     }
 }
 
+int get_canvas_pixel(VGA_WINDOW * wnd, int x, int y)
+{
+    return *(wnd->canvas.buffer + y * wnd->canvas.bound.width + x);
+}
+
+void set_canvas_pixel(VGA_WINDOW * wnd, int x, int y, int color)
+{
+    if(x < 0 && x > wnd->canvas.bound.width-1)
+        return;
+    if(y < 0 && y > wnd->canvas.bound.height-1)  
+        return;
+    *(wnd->canvas.buffer + y * wnd->canvas.bound.width + x) = color;
+}
+
 /********************************************************************************
  *                             QUADTREE FUNCTIONS                               *
  * *****************************************************************************/
@@ -404,9 +578,10 @@ BOUND create_bound(int x, int y, int width, int height)
 
 int bound_contains(BOUND * b, int x, int y)
 {
-    if(x >= b->x && x < b->x+b->width
-        && y >= b->y && y < b->y+b->height) {
-        return 1;
+    if( (x >= b->x) && (x < b->x+b->width) ) {
+        if( (y >= b->y) && (y < b->y+b->height) ) {
+            return 1;
+        }
     }
     return 0;
 }
@@ -422,9 +597,20 @@ int bound_intersects(BOUND * a, BOUND * b)
     return 1;
 }
 
+int bound_contains_within(BOUND * in, BOUND * out)
+{
+    if( (in->x >= out->x) && (in->x+in->width <= out->x+out->width) ) {
+        if( (in->y >= out->y) && (in->y+in->height <= out->y+out->height) )
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 BOUND get_intersection(BOUND * a, BOUND * b)
 {
-    BOUND intr = {0,0,0,0};
+    BOUND intr;
 
     // find x
     if(a->x >= b->x)
@@ -517,7 +703,6 @@ int search_qtree(QNODE * q, int x, int y)
         if (q->se && bound_contains(&(q->se->bound), x, y))
             return search_qtree(q->se, x, y);
     }
-
     return 0;
 }
 
@@ -534,69 +719,52 @@ void subdivide(QNODE * q, BOUND b)
     BOUND sw = create_bound(x,y+(h/2),(w/2),(h/2));
     BOUND se = create_bound(x+(w/2),y+(h/2),(w/2),(h/2));
 
-    if(!q->nw && bound_intersects(&(nw),&b)) {
-        q->children++;
-        q->nw = create_qnode(nw.x,nw.y,nw.width,nw.height);
+    
+    if (bound_intersects(&(nw),&b)) {
+        if(!q->nw) {
+            q->children++;
+            q->nw = create_qnode(nw.x,nw.y,nw.width,nw.height);
+        }
         check_qnode(q->nw, b);
     }
 
-    if(!q->ne && bound_intersects(&(ne),&b)) {
-        q->children++;
-        q->ne = create_qnode(ne.x,ne.y,ne.width,ne.height);
+    if (bound_intersects(&(ne),&b)) {
+        if(!q->ne) {
+            q->children++;
+            q->ne = create_qnode(ne.x,ne.y,ne.width,ne.height);
+        }
         check_qnode(q->ne, b);
     }
 
-    if(!q->sw && bound_intersects(&(sw),&b)) {
-        q->children++;
-        q->sw = create_qnode(sw.x,sw.y,sw.width,sw.height);
+    if (bound_intersects(&(sw),&b)) {
+        if(!q->sw) {
+            q->children++;
+            q->sw = create_qnode(sw.x,sw.y,sw.width,sw.height);
+        }
         check_qnode(q->sw, b);
     }
 
-    if(!q->se && bound_intersects(&(se),&b)) {
-        q->children++;
-        q->se = create_qnode(se.x,se.y,se.width,se.height);
+    if (bound_intersects(&(se),&b)) {
+        if(!q->se) {
+            q->children++;
+            q->se = create_qnode(se.x,se.y,se.width,se.height);
+        }
         check_qnode(q->se, b);
     }
-
 }
 
 void check_qnode(QNODE * q, BOUND b)
 {
-    if(q->bound.width <= 1 || q->bound.height <= 1) {
+    if(bound_contains_within(&(q->bound), &(b))) {
         q->hidden = 1;
     }
     else if(bound_intersects(&(q->bound), &b)) {
         subdivide(q, b);
     }
-
-    if(!q->nw->hidden)
-        if(bound_intersects(&(q->nw->bound), &b))
-            check_qnode(q->nw, b);
-    if(!q->ne->hidden)       
-        if(bound_intersects(&(q->ne->bound), &b))
-            check_qnode(q->ne, b);
-    if(!q->sw->hidden)
-        if(bound_intersects(&(q->sw->bound), &b))
-            check_qnode(q->sw, b);
-    if(!q->se->hidden)
-        if(bound_intersects(&(q->se->bound), &b))
-            check_qnode(q->se, b);
-    
-    if(q->children == 4) {
-        if(q->nw->hidden == 1 &&
-            q->ne->hidden == 1 &&
-            q->sw->hidden == 1 &&
-            q->se->hidden == 1)
-        {
-            destroy_children(q);
-            q->hidden = 1;
-        }
-    }
 }
 
 void reset_qtrees()
 {
-
     VGA_WINDOW * w_ptr = window_list_head;
 
     while(w_ptr != NULL){
@@ -619,9 +787,114 @@ void build_quadtrees()
 
         f_ptr = w_ptr->prev;
         while(f_ptr != NULL) {
-            check_qnode(w_ptr->root, get_intersection(&(w_ptr->frame.bound), &(f_ptr->frame.bound)));
+            check_qnode(w_ptr->root, 
+                get_intersection(
+                    &(w_ptr->frame.bound), 
+                    &(f_ptr->frame.bound)));
             f_ptr = f_ptr->prev;
         }
         w_ptr = w_ptr->prev;
     }
+}
+
+
+/********************************************************************************
+ *                                TEST PROCESS                                  *
+ * *****************************************************************************/
+
+void vga_test (PROCESS proc, PARAM param)
+{
+	VGA_WINDOW_MSG msg;
+
+	// Create Window 1
+	msg.cmd = VGA_CREATE_WINDOW;
+	msg.u.create_window.title = "Window 1";
+	msg.u.create_window.x = 50;
+	msg.u.create_window.y = 50;
+	msg.u.create_window.width = 200;
+	msg.u.create_window.height = 50;
+	send(vga_port, &msg);
+	unsigned int window1_id = msg.u.create_window.window_id;
+
+	// Create Window 2
+	msg.cmd = VGA_CREATE_WINDOW;
+	msg.u.create_window.title = "Window 2";
+	msg.u.create_window.x = 140;
+	msg.u.create_window.y = 70;
+	msg.u.create_window.width = 70;
+	msg.u.create_window.height = 120;
+	send(vga_port, &msg);
+	unsigned int window2_id = msg.u.create_window.window_id;
+
+	// Create Window 3
+	msg.cmd = VGA_CREATE_WINDOW;
+	msg.u.create_window.title = "Window 3";
+	msg.u.create_window.x = 150;
+	msg.u.create_window.y = 60;
+	msg.u.create_window.width = 50;
+	msg.u.create_window.height = 50;
+	send(vga_port, &msg);
+	unsigned int window3_id = msg.u.create_window.window_id;
+/*
+	// Draw some lines in Window 1
+	char current_color = 0;
+	for (int x = 0; x < 100; x += 2) {
+		msg.cmd = VGA_DRAW_LINE;
+		msg.u.draw_line.window_id = window1_id;
+		msg.u.draw_line.x0 = x;
+		msg.u.draw_line.y0 = 0;
+		msg.u.draw_line.x1 = 100 - x;
+		msg.u.draw_line.y1 = 49;
+		msg.u.draw_line.color = current_color++;
+		send(vga_port, &msg);
+	}
+
+	// Write some text in Window 2
+	msg.cmd = VGA_DRAW_TEXT;
+	msg.u.draw_text.window_id = window2_id;
+	msg.u.draw_text.text = "Hello CSC720!";
+	msg.u.draw_text.x = 1;
+	msg.u.draw_text.y = 1;
+	msg.u.draw_text.fg_color = 0x3f; // White
+	msg.u.draw_text.bg_color = 0;
+	send(vga_port, &msg);
+
+	// Write some text in Window 2 that will be clipped
+	msg.cmd = VGA_DRAW_TEXT;
+	msg.u.draw_text.window_id = window2_id;
+	msg.u.draw_text.text = "Text that is too long will be clipped";
+	msg.u.draw_text.x = 20;
+	msg.u.draw_text.y = 20;
+	msg.u.draw_text.fg_color = 0x3f; // White
+	msg.u.draw_text.bg_color = 0;
+	send(vga_port, &msg);
+
+	// Draw some random pixels in Window 3
+	msg.cmd = VGA_DRAW_PIXEL;
+	msg.u.draw_pixel.window_id = window3_id;
+	current_color = 0;
+	for (int x = 3; x < 100; x += 5) {
+		for (int y = 3; y < 100; y += 5) {
+		msg.u.draw_pixel.x = x;
+		msg.u.draw_pixel.y = y;
+		msg.u.draw_pixel.color = current_color;
+		current_color = (current_color + 1) % 64;
+		send(vga_port, &msg);
+		}
+	}
+
+    msg.cmd = VGA_DRAW_PIXEL;
+	msg.u.draw_pixel.window_id = window2_id;
+	current_color = 0;
+	for (int x = 3; x < 100; x += 5) {
+		for (int y = 3; y < 100; y += 5) {
+		msg.u.draw_pixel.x = x;
+		msg.u.draw_pixel.y = y;
+		msg.u.draw_pixel.color = current_color;
+		current_color = (current_color + 1) % 64;
+		send(vga_port, &msg);
+		}
+	}
+*/
+    become_zombie();
 }
